dfd4d4920dbf810cad55c9c35d36df1c3b203738
nancy_wan <nancy_wan@thecdm.ca>
2023-10-31 20:49:46 -0700

    Hooking up Brochure finish reading with new dialogue

--- Assets/Scripts/InteractObj/InteractionManager.cs
+++ Assets/Scripts/InteractObj/InteractionManager.cs
@@ -1,39 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class InteractionManager : MonoBehaviour
 {
-    //
-
-    //BrochureÐû´«ÊÖ²á
+    //Brochure
     [SerializeField] private string[] names;
     [SerializeField] private string[] informations;
     [SerializeField] private string[] introductions;
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private TextMeshProUGUI informationText;
     [SerializeField] private TextMeshProUGUI introductionText;
+    [SerializeField] private UnityEvent onFinishReadingBrochure;
     private int index;
     private void Start()
     {
         index = 0;
     }
+    
     public void FlipBrochure()
     {
-        if(index<names.Length-1)
+        if (index < names.Length - 1)
         {
             index += 1;
             nameText.text = names[index];
             informationText.text = informations[index];
             introductionText.text = introductions[index];
         }
-        
     }
+
+    public void CloseBrochure()
+    {
+        if (index >= names.Length - 1)
+        {
+            onFinishReadingBrochure?.Invoke();
+        }
+
+        InitiateBrochure();
+    }
+    
     public void InitiateBrochure()
     {
-        index=0;
+        index = 0;
         nameText.text = names[index];
         informationText.text = informations[index];
         introductionText.text = introductions[index];
