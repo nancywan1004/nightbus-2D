7d489ecaae8aa4f456160fefdebaa4fc54e1b6a9
nancy_wan <nancy_wan@thecdm.ca>
2023-11-19 16:10:10 -0800

    Adding SecondDay and a hot-transition button in FirstDay and making Global_Interaction_UI persistent

---
+++ Assets/Scripts/UI/GlobalInteractionUIManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GlobalInteractionUIManager : MonoBehaviour
+{
+    [SerializeField] private Button _secondDaySceneSwitchButton;
+
+    private const string SECOND_DAY_SCENE_NAME = "SecondDay";
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        SetupUIBindings();
+    }
+
+    private void SetupUIBindings()
+    {
+        _secondDaySceneSwitchButton.onClick.AddListener(LoadSecondDayScene);
+    }
+
+    private void LoadSecondDayScene()
+    {
+        SceneManager.LoadSceneAsync("SecondDay", LoadSceneMode.Single);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+    {
+        if (string.Equals(scene.name, SECOND_DAY_SCENE_NAME))
+        {
+            _secondDaySceneSwitchButton.gameObject.SetActive(false);
+        }
+    }
+}
